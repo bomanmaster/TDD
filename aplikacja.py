c6383d02784bfcc3b220593f44eb21498396a8df
bomanmaster <bomanmaster@gmail.com>
2015-05-26 16:16:34 +0200

    Dokonano refaktoryzacji pierwszej funkcji w pliku aplikacja.py. Kod zostal uporzadkowany, ujednolicono nazewnictwo, dodano opis funkcji. Po refaktoryzacji wynik testu nie zmnienil sie (pozostal pozytywny)

--- aplikacja.py
+++ aplikacja.py
@@ -5,10 +5,24 @@ class Sum_of_squares_diffrence:
     def __init__(self):
         pass
 
-    def sum_squares(self):
-        suma = 0
-        for i in range(1, 11, 1):
-            sum = pow(i, 2)
-            suma += sum
+# Squares first 10 natural numbers and adds them
+#
+# sum - sum of squared numbers
+# pow_sum - sum of individual squared number
+#
+#   example
+#
+#   sum of squares of 2 natural numbers (1,2)
+#   # => 5
+#
+# Returns the sum of the squared numbers(first 10 natural numbers)
 
-        return suma
+    def sum_squares(self):
+
+        sum = 0
+
+        for i in range(1, 11, 1):
+            pow_sum = pow(i, 2)
+            sum += pow_sum
+
+        return sum
