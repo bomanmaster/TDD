352e4436ff2cb8131474b7d94b62a2988c32e6e6
bomanmaster <bomanmaster@gmail.com>
2015-05-26 15:58:17 +0200

    Dodanie pliku z testem pierwszej funkcji. Test zwraca blad z powodu braku funkcji

---
+++ aplikacja_test.py
@@ -0,0 +1,18 @@
+__author__ = 'Konrad'
+__author__ = 'Konrad'
+
+import unittest
+
+
+
+class Test_sum_of_squares_diffrence(unittest.TestCase):
+
+    def test_sum_of_squares_(self):
+        self.assertEqual(aplikacja.Sum_of_squares_diffrence.sum_squares(self), 385)
+
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(Test_sum_of_squares_diffrence)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+
